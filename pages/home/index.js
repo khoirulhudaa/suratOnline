ed256c0a88232109fdd877e2f5b4682ffa5476b3
Degovan <developervalclass@gmail.com>
2021-12-02 14:21:33 +0700

    surat online - reactjs

---
+++ pages/home/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function index() {
+  return (
+    <div className="flex h-screen align-center justify-center">
+      <h1>Halaman Untuk Homepage</h1>
+    </div>
+  );
+}
+
+export default index;
